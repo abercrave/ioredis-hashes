ef0703fc026acc559b6635b27e6572e8ec448a08
Ben Evans <ben.evans@chghealthcare.com>
2021-07-20 16:24:01 -0600

    Comment updates

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const isPlainObject = require('lodash.isplainobject')
 const redis = new Redis()
 
 /**
- * Here's the built-in argument transformer converting:
+ * Use the built-in argument transformer to convert the second argument in the HMSET command:
  * 
- * hmset('key', { k1: 'v1', k2: 'v2' })
+ *   hmset('key', { k1: 'v1', k2: 'v2' })
+ * 
  * or:
- * hmset('key', new Map([['k1', 'v1'], ['k2', 'v2']]))
+ * 
+ *    hmset('key', new Map([['k1', 'v1'], ['k2', 'v2']]))
  * 
  * into:
  * 
- * hmset('key', 'k1', 'v1', 'k2', 'v2')
+ *   hmset('key', 'k1', 'v1', 'k2', 'v2')
  */
 Redis.Command.setArgumentTransformer("hmset", args => {
   if (args.length === 2) {
@@ -27,7 +29,7 @@ Redis.Command.setArgumentTransformer("hmset", args => {
       ]
     }
 
-    if (typeof value === "object" && value !== null) {
+    if (value && typeof value === "object") {
       const convertedObj = convertObjectToArray(value).map(item => {
         if (isPlainObject(item)) {
           return JSON.stringify(item)
@@ -47,16 +49,17 @@ Redis.Command.setArgumentTransformer("hmset", args => {
 })
 
 /**
- * Here's the built-in reply transformer converting the HGETALL reply:
+ * Use the built-in reply transformer to convert the HGETALL reply:
  *
- * ['k1', 'v1', 'k2', 'v2']
+ *   ['k1', 'v1', 'k2', 'v2']
  * 
  * into:
  * 
- * { k1: 'v1', 'k2': 'v2' }
+ *   { k1: 'v1', 'k2': 'v2' }
  */
 Redis.Command.setReplyTransformer("hgetall", result => {
   if (Array.isArray(result)) {
+    // Attempt to parse each item as JSON or, on failure, return it as-is
     const parsedResult = result.map(item => {
       try {
         return JSON.parse(item)
@@ -65,12 +68,64 @@ Redis.Command.setReplyTransformer("hgetall", result => {
       }
     })
 
+    // Use the internal ioredis util to return the result as an object literal
     return packObject(parsedResult)
   }
 
   return result
 })
 
+/**
+ * 
+ * @param {array} arr - The array of objects
+ * @param {string} key - The key under which each child object will be stored in the resulting 
+ *   object. If said key is not defined on the child it will default to its 
+ *   numerical index in the array
+ * @returns {object} 
+ */
+function convertArrayToObjectByKey(arr, key) {
+  return arr.reduce((acc, item) => {
+    acc[(isPlainObject(item) && item[key]) || index] = item
+    return acc
+  }, {})
+}
+
+/**
+ * Sets an array of object literals as a hash of fields identified by the supplied key
+ * 
+ * @param {string} key - The key under which the hash will be stored
+ * @param {array} arr - The array of objects
+ */
+async function setArrayAsHash(key, arr) {
+  const hash = convertArrayToObjectByKey(arr, 'id')
+
+  console.log('\n******* setArrayAsHash *******')
+  console.log(hash)
+  console.log('***********************\n')
+
+  await redis.hmset(key, hash)
+}
+
+/**
+ * Returns a Redis hash as an array
+ * 
+ * @param {string} key - The key of the hash to retrieve
+ * @returns {Array} The transformed hash
+ */
+async function getHashAsArray(key) {
+  const hash = await redis.hgetall(key)
+  const result = Object.values(hash)
+
+  console.log('\n******* getHashAsArray *******')
+  console.log(result)
+  console.log('***********************\n')
+
+  return result
+}
+
+/**
+ * An array of object literals
+ */
 const data = [
   {
     id: 1234,
@@ -88,37 +143,15 @@ const data = [
   }
 ]
 
-function convertArrayToObjectByKey(arr, key) {
-  return arr.reduce((acc, item) => {
-    acc[item[key] || index] = item
-    return acc
-  }, {})
-}
-
-async function saveArrayAsHash(key, arr) {
-  const hash = convertArrayToObjectByKey(arr, 'id')
-
-  console.log('\n******* saveArrayAsHash: *******')
-  console.log(hash)
-  console.log('***********************\n')
-
-  await redis.hmset(key, hash)
-}
-
-async function getHash(key) {
-  const result = await redis.hgetall(key)
-  return Object.values(result)
-}
-
 async function main() {
+  // Clear the cache on each run
   await redis.flushall()
 
-  await saveArrayAsHash('testHash', data)
+  // Save the data array under the testHash key
+  await setArrayAsHash('testHash', data)
 
-  const result = await getHash('testHash')
-  console.log('\n******* getHash Result: *******')
-  console.log(result)
-  console.log('***********************\n')
+  // Get the contents of the testHash as an array
+  await getHashAsArray('testHash')
 }
 
 main()
